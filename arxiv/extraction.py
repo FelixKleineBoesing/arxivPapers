660b6436f0d9eb1485ff9771a8bc2da308175f54
Felix <felix.boesing@t-online.de>
2021-07-18 20:43:38 +0200

    fix workflow

--- arxiv/extraction.py
+++ arxiv/extraction.py
@@ -2,11 +2,9 @@ from ogb.nodeproppred import NodePropPredDataset
 from pathlib import Path
 
 
-def download_arxiv_dataset(download_dir: Path =Path("data",  "raw")):
-    dataset = NodePropPredDataset(name="ogbg-arxiv", root="data/")
-
-
+def download_arxiv_dataset(download_dir: Path = Path("..", "data",  "raw")):
+    dataset = NodePropPredDataset(name="ogbn-arxiv", root=download_dir)
 
 
 if __name__ == "__main__":
-    download_arxiv_dataset()
+    download_arxiv_dataset(Path("..", "data",  "raw"))
