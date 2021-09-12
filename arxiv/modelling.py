79311fe61e0252aedc56e80005e144fc211393e3
Felix <felix.boesing@t-online.de>
2021-09-12 15:16:50 +0200

    MINOR CHANGES

--- arxiv/modelling.py
+++ arxiv/modelling.py
@@ -7,14 +7,7 @@ from tensorflow.python.keras.optimizer_v2.adam import Adam
 
 
 def build_model(number_nodes:  int, number_features: int, num_classes: int, channels: int = 256,
-                dropout: float =  0.4, compile_args:  dict = None):
-    compile_args = {} if compile_args is None else compile_args
-    if "optimizer" not in compile_args:
-        compile_args["optimizer"] = Adam()
-    if "loss" not in compile_args:
-        compile_args["loss"] = SparseCategoricalCrossentropy()
-    if "metrics" not in compile_args:
-        compile_args["metrics"] = ["accuracy"]
+                dropout: float =  0.4):
 
     x_inp = Input(shape=(number_features,))
     a_inp = Input((number_nodes,), sparse=True)
@@ -25,15 +18,26 @@ def build_model(number_nodes:  int, number_features: int, num_classes: int, chan
     x_2 = BatchNormalization()(x_2)
     x_2 = Dropout(dropout)(x_2)
     predictions = GCNConv(num_classes, activation="softmax")([x_2, a_inp])
-    model = tf.keras.Model(inputs=x_inp, outputs= predictions)
-    model.compile(**compile_args)
-    return model
+    model = tf.keras.Model(inputs=[x_inp, a_inp], outputs= predictions)
+    optimizer = Adam(0.5e-2)
+    loss = SparseCategoricalCrossentropy()
+    return model, optimizer, loss
 
 
-def train_model(model, generator, train_label, train_idx, test_label, test_idx):
+def train_model(model, train_label, train_idx, test_label, test_idx):
     model.fit(generator.flow(train_idx, train_label), epochs=5)
 
     loss, accuracy = model.evaluate(generator.flow(test_idx, test_label))
     return loss,  accuracy
 
 
+def get_train_function(model, optimizer, loss_func):
+    @tf.function
+    def train(inputs, target, mask):
+        with tf.GradientTape() as tape:
+            predictions = model(inputs, training=True)
+            loss = loss_func(target[mask], predictions[mask]) + sum(model.losses)
+
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        return loss
